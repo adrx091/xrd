9614bbe4e9c56d6403b42b5faac247aeb7623be7
unknown <adryanviniciusrt98@gmail.com>
2021-11-30 22:37:45 -0300

    sla

---
+++ index.js
@@ -0,0 +1,15 @@
+//IMPORTS
+const puppeteer = require('puppeteer');
+const axios = require('axios');
+const read = require('readline-sync');
+//const chalk = require('chalk');
+const fs = require('fs');
+//IMPORTS
+
+//CONSTS MAE
+
+const log = console.log;
+const limpar = console.clear;
+const painel = require('./lib/painel.js');
+
+//CONSTS MAE
